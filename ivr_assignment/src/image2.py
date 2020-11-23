d43f324ca712865e1ee792b5a3d5c219b08cca36
aforbes23 <43933551+aforbes23@users.noreply.github.com>
2020-11-23 20:04:13 +0000

    Initial commit of assignment folder as given
    Added joint movement for part 2

---
+++ ivr_assignment/src/image2.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float64MultiArray, Float64
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class image_converter:
+
+  # Defines publisher and subscriber
+  def __init__(self):
+    # initialize the node named image_processing
+    rospy.init_node('image_processing', anonymous=True)
+    # initialize a publisher to send images from camera2 to a topic named image_topic2
+    self.image_pub2 = rospy.Publisher("image_topic2",Image, queue_size = 1)
+    # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
+    self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw",Image,self.callback2)
+    # initialize the bridge between openCV and ROS
+    # initialize a publisher for the robot joint controllers
+    self.robot_joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
+    self.robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
+    self.robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
+    # initialize the bridge between openCV and ROS
+    self.bridge = CvBridge()
+    # get times
+    self.time_trajectory = rospy.get_time()
+
+  def joint_trajectories(self):
+    cur_time = np.array([rospy.get_time() - self.time_trajectory])
+    joint2 = (np.pi/2) * np.sin( (np.pi/15) * cur_time)
+    joint3 = (np.pi/2) * np.sin( (np.pi/18) * cur_time)
+    joint4 = (np.pi/2) * np.sin( (np.pi/20) * cur_time)
+
+    return joint2, joint3, joint4
+
+
+  # Recieve data, process it, and publish
+  def callback2(self,data):
+    # Recieve the image
+    try:
+      self.cv_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+    # Uncomment if you want to save the image
+    #cv2.imwrite('image_copy.png', cv_image)
+
+    joint2, joint3, joint4 = self.joint_trajectories()
+
+    
+    im2=cv2.imshow('window2', self.cv_image2)
+    cv2.waitKey(1)
+
+    # Publish the results
+    try: 
+      self.image_pub2.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
+      self.robot_joint2_pub.publish(joint2)
+      self.robot_joint3_pub.publish(joint3)
+      self.robot_joint4_pub.publish(joint4)
+    except CvBridgeError as e:
+      print(e)
+
+# call the class
+def main(args):
+  ic = image_converter()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+# run the code if the node is called
+if __name__ == '__main__':
+    main(sys.argv)
+
+

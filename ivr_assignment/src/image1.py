e7977bae97b40725396f6427e8488266d47c0c9e
aforbes23 <43933551+aforbes23@users.noreply.github.com>
2020-11-24 16:15:00 +0000

    Changed image1 and image2 to calculate angles
    Calculation for joint 4 still dodgy
    No comments as of yet

--- ivr_assignment/src/image1.py
+++ ivr_assignment/src/image1.py
@@ -25,18 +25,97 @@ class image_converter:
     self.robot_joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
     self.robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
     self.robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
+    # initialize a subscirber for joint angles from image2
+    self.joint_angles_sub = rospy.Subscriber("/joints2/", Float64MultiArray, self.angle_listener)
+    # initialize a publisher for final joint angle estimate output
+    self.estimated_angles_pub = rospy.Publisher("estimated_joints", Float64MultiArray, queue_size=10)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
     # get times
     self.time_trajectory = rospy.get_time()
 
-  def joint_trajectories(self):
-    cur_time = np.array([rospy.get_time() - self.time_trajectory])
-    joint2 = (np.pi/2) * np.sin( (np.pi/15) * cur_time)
-    joint3 = (np.pi/2) * np.sin( (np.pi/18) * cur_time)
-    joint4 = (np.pi/2) * np.sin( (np.pi/20) * cur_time)
+  def detect_yellow(self):
+    cv_image_hsv = cv2.cvtColor(self.cv_image1, cv2.COLOR_BGR2HSV)
+    joint = cv2.inRange(cv_image_hsv, (24,50,50), (34,255,255))
+    kernel = np.ones((5,5), np.uint8)
+    joint = cv2.dilate(joint, kernel, iterations = 3)
+    moments = cv2.moments(joint)
+    x = int(moments['m10']/moments['m00'])
+    y = int(moments['m01']/moments['m00'])
+    
+    return np.array([x,y])
 
-    return joint2, joint3, joint4
+  def detect_blue(self):
+    cv_image_hsv = cv2.cvtColor(self.cv_image1, cv2.COLOR_BGR2HSV)
+    joint = cv2.inRange(cv_image_hsv, (97,50,50), (121,255,255))
+    kernel = np.ones((5,5), np.uint8)
+    joint = cv2.dilate(joint, kernel, iterations = 3)
+    moments = cv2.moments(joint)
+    x = int(moments['m10']/moments['m00'])
+    y = int(moments['m01']/moments['m00'])
+    
+    return np.array([x,y])
+
+  def detect_green(self):
+    cv_image_hsv = cv2.cvtColor(self.cv_image1, cv2.COLOR_BGR2HSV)
+    joint = cv2.inRange(cv_image_hsv, (40,50,50), (63,255,255))
+    kernel = np.ones((5,5), np.uint8)
+    joint = cv2.dilate(joint, kernel, iterations = 3)
+    moments = cv2.moments(joint)
+    x = int(moments['m10']/moments['m00'])
+    y = int(moments['m01']/moments['m00'])
+    
+    return np.array([x,y])
+    
+
+  def detect_red(self):
+    cv_image_hsv = cv2.cvtColor(self.cv_image1, cv2.COLOR_BGR2HSV)
+    joint = cv2.inRange(cv_image_hsv, (0,50, 50), (5,255,255))
+    kernel = np.ones((5,5), np.uint8)
+    joint = cv2.dilate(joint, kernel, iterations = 3)
+    moments = cv2.moments(joint)
+    x = int(moments['m10']/moments['m00'])
+    y = int(moments['m01']/moments['m00'])
+    
+    return np.array([x,y])
+
+  def pixel_to_meter(self, yellow_centre, blue_centre):
+    link_length_p = np.linalg.norm(blue_centre - yellow_centre)
+    con_factor = 3/link_length_p
+    return con_factor
+
+  def convert_centres(self, centres_p):
+    centres_m = []
+    centres_m.append(np.array([0,0]))
+    con_factor = self.pixel_to_meter(centres_p[0], centres_p[1])
+
+    for centre in centres_p[1:]:
+        x = (centre[0] - centres_p[0][0])*con_factor
+        y = (abs(centre[1] - centres_p[0][1]))*con_factor
+        centres_m.append(np.array([x,y]))
+    
+    return centres_m
+
+  def find_angles(self, centres_m):
+    angles = []
+    for i in range(1,len(centres_m)):
+      opp = centres_m[i-1][0] - centres_m[i][0]
+      adj = centres_m[i][1] - centres_m[i-1][1]
+      angle = np.arctan2(opp, adj)
+      angles.append(angle)
+  
+    return np.array(angles)
+  
+  def find_final_angles(self):
+    joint1 = self.im1_angles[1]
+    joint2 = self.im2_angles[1]
+    joint3 = self.im1_angles[2] + self.im2_angles[2] - joint1 - joint2
+    return np.array([joint1, joint2, joint3])
+
+  
+  # Recieve angles from image2.py
+  def angle_listener(self, arr):
+    self.im2_angles = np.array(arr.data)
 
 
   # Recieve data from camera 1, process it, and publish
@@ -46,6 +125,21 @@ class image_converter:
       self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
+
+    yellow = self.detect_yellow()
+    blue = self.detect_blue()
+    green = self.detect_green()
+    red = self.detect_red()
+
+    centres_p = [yellow, blue, green, red]
+    centres_m = self.convert_centres(centres_p)
+    self.im1_angles = self.find_angles(centres_m)
+
+    self.estimated_angles = Float64MultiArray()
+    self.estimated_angles.data = np.array([0.0,0.0,0.0])
+    self.estimated_angles.data = self.find_final_angles()
+
+
     
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
@@ -55,6 +149,7 @@ class image_converter:
     # Publish the results
     try: 
       self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
+      self.estimated_angles_pub.publish(self.estimated_angles)
     except CvBridgeError as e:
       print(e)
 
--- ivr_assignment/src/image2.py
+++ ivr_assignment/src/image2.py
@@ -21,11 +21,14 @@ class image_converter:
     self.image_pub2 = rospy.Publisher("image_topic2",Image, queue_size = 1)
     # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
     self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw",Image,self.callback2)
-    # initialize the bridge between openCV and ROS
     # initialize a publisher for the robot joint controllers
+    self.robot_joint1_pub = rospy.Publisher("/robot/joint1_position_controller/command", Float64, queue_size=10)
     self.robot_joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
     self.robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
     self.robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
+    # initialize publisher and subscriber for joint angles and actual angles
+    self.joint_angles_pub = rospy.Publisher("joints2", Float64MultiArray, queue_size=10)
+    self.control_angles_pub = rospy.Publisher("control_angles", Float64MultiArray, queue_size=10)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
     # get times
@@ -35,12 +38,94 @@ class image_converter:
     cur_time = np.array([rospy.get_time() - self.time_trajectory])
     joint2 = (np.pi/2) * np.sin( (np.pi/15) * cur_time)
     joint3 = (np.pi/2) * np.sin( (np.pi/18) * cur_time)
-    joint4 = (np.pi/2) * np.sin( (np.pi/20) * cur_time)
+    # changed to avoid hitting the ground
+    joint4 = (np.pi/3) * np.sin( (np.pi/20) * cur_time)
 
-    return joint2, joint3, joint4
+    return np.array([joint2, joint3, joint4])
+
+  def publish_joint_trajectories(self):
+    self.robot_joint1_pub.publish(0.0)
+    self.robot_joint2_pub.publish(self.control_angles.data[0])
+    self.robot_joint3_pub.publish(self.control_angles.data[1])
+    self.robot_joint4_pub.publish(self.control_angles.data[2])
+    self.control_angles_pub.publish(self.control_angles)
+    
+
+  def detect_yellow(self):
+    cv_image_hsv = cv2.cvtColor(self.cv_image2, cv2.COLOR_BGR2HSV)
+    joint = cv2.inRange(cv_image_hsv, (24,50,50), (34,255,255))
+    kernel = np.ones((5,5), np.uint8)
+    joint = cv2.dilate(joint, kernel, iterations = 3)
+    moments = cv2.moments(joint)
+    x = int(moments['m10']/moments['m00'])
+    y = int(moments['m01']/moments['m00'])
+    
+    return np.array([x,y])
+
+  def detect_blue(self):
+    cv_image_hsv = cv2.cvtColor(self.cv_image2, cv2.COLOR_BGR2HSV)
+    joint = cv2.inRange(cv_image_hsv, (97,50,50), (121,255,255))
+    kernel = np.ones((5,5), np.uint8)
+    joint = cv2.dilate(joint, kernel, iterations = 3)
+    moments = cv2.moments(joint)
+    x = int(moments['m10']/moments['m00'])
+    y = int(moments['m01']/moments['m00'])
+    
+    return np.array([x,y])
+
+  def detect_green(self):
+    cv_image_hsv = cv2.cvtColor(self.cv_image2, cv2.COLOR_BGR2HSV)
+    joint = cv2.inRange(cv_image_hsv, (40,50,50), (63,255,255))
+    kernel = np.ones((5,5), np.uint8)
+    joint = cv2.dilate(joint, kernel, iterations = 3)
+    moments = cv2.moments(joint)
+    x = int(moments['m10']/moments['m00'])
+    y = int(moments['m01']/moments['m00'])
+    
+    return np.array([x,y])
+    
+
+  def detect_red(self):
+    cv_image_hsv = cv2.cvtColor(self.cv_image2, cv2.COLOR_BGR2HSV)
+    joint = cv2.inRange(cv_image_hsv, (0,50, 50), (5,255,255))
+    kernel = np.ones((5,5), np.uint8)
+    joint = cv2.dilate(joint, kernel, iterations = 3)
+    moments = cv2.moments(joint)
+    x = int(moments['m10']/moments['m00'])
+    y = int(moments['m01']/moments['m00'])
+    
+    return np.array([x,y])
+
+    
+  def pixel_to_meter(self, yellow_centre, blue_centre):
+    link_length_p = np.linalg.norm(blue_centre - yellow_centre)
+    con_factor = 3/link_length_p
+    return con_factor
+
+  def convert_centres(self, centres_p):
+    centres_m = []
+    centres_m.append(np.array([0,0]))
+    con_factor = self.pixel_to_meter(centres_p[0], centres_p[1])
+
+    for centre in centres_p[1:]:
+        x = (centre[0] - centres_p[0][0])*con_factor
+        y = (abs(centre[1] - centres_p[0][1]))*con_factor
+        centres_m.append(np.array([x,y]))
+    
+    return centres_m
+
+  def find_angles(self, centres_m):
+    angles = []
+    for i in range(1,len(centres_m)):
+      opp = centres_m[i-1][0] - centres_m[i][0]
+      adj = centres_m[i][1] - centres_m[i-1][1]
+      angle = -np.arctan2(opp, adj)
+      angles.append(angle)
+  
+    return np.array(angles)
 
 
-  # Recieve data, process it, and publish
+# Recieve data, process it, and publish
   def callback2(self,data):
     # Recieve the image
     try:
@@ -50,18 +135,32 @@ class image_converter:
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
 
-    joint2, joint3, joint4 = self.joint_trajectories()
+    self.control_angles = Float64MultiArray()
+    self.control_angles.data = np.array([0.0,0.0,0.0])
+    self.control_angles.data = self.joint_trajectories()
 
-    
+    self.publish_joint_trajectories()
+
+
+    self.joints = Float64MultiArray()
+    self.joints.data = np.array([0.0, 0.0, 0.0])
+
+    yellow = self.detect_yellow()
+    blue = self.detect_blue()
+    green = self.detect_green()
+    red = self.detect_red()
+
+    centres_p = [yellow, blue, green, red]
+    centres_m = self.convert_centres(centres_p)
+    self.joints.data = self.find_angles(centres_m)
+
     im2=cv2.imshow('window2', self.cv_image2)
     cv2.waitKey(1)
 
     # Publish the results
     try: 
       self.image_pub2.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
-      self.robot_joint2_pub.publish(joint2)
-      self.robot_joint3_pub.publish(joint3)
-      self.robot_joint4_pub.publish(joint4)
+      self.joint_angles_pub.publish(self.joints)
     except CvBridgeError as e:
       print(e)
 
